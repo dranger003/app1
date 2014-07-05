a84385c52a69378561ccba78f4c28164e9701051
Daniel Ranger <daniel@formatique.com>
2014-07-05 07:57:48 -0400

    Indents, redux.

--- app1.cpp
+++ app1.cpp
@@ -6,99 +6,99 @@
 
 typedef struct
 {
-	int p;
-	int v;
+    int p;
+    int v;
 } pv_t;
 
 typedef struct
 {
-	pv_t pvs[5];
-	int v;
+    pv_t pvs[5];
+    int v;
 } ctrl_t;
 
 ctrl_t ctrls[] =
 {
-	{
-		{
-			{ A0, 155 }, // R
-			{ A1, 247 }, // G
-			{ A4, 120 }, // B
-			{ D0, LOW }, // S
-			{ D1, LOW }, // R
-		},
-		LOW,
-	},
-	{
-		{
-			{ A5, 155 }, // R
-			{ A6, 247 }, // G
-			{ A7, 120 }, // B
-			{ D2, LOW }, // S
-			{ D3, LOW }, // R
-		},
-		LOW,
-	},
+    {
+        {
+            { A0, 155 }, // R
+            { A1, 247 }, // G
+            { A4, 120 }, // B
+            { D0, LOW }, // S
+            { D1, LOW }, // R
+        },
+        LOW,
+    },
+    {
+        {
+            { A5, 155 }, // R
+            { A6, 247 }, // G
+            { A7, 120 }, // B
+            { D2, LOW }, // S
+            { D3, LOW }, // R
+        },
+        LOW,
+    },
 };
 
 void reset()
 {
-	for (int c = 0; c < sizeof(ctrls) / sizeof(ctrl_t); c++)
-	{
-		analogWrite(ctrls[c].pvs[BLUE].p, ctrls[c].pvs[BLUE].v);
+    for (int c = 0; c < sizeof(ctrls) / sizeof(ctrl_t); c++)
+    {
+        analogWrite(ctrls[c].pvs[BLUE].p, ctrls[c].pvs[BLUE].v);
 
-		for (int pv = 0; pv < sizeof(ctrls[c].pvs) / sizeof(pv_t); pv++)
-		{
-			int p = ctrls[c].pvs[pv].p;
+        for (int pv = 0; pv < sizeof(ctrls[c].pvs) / sizeof(pv_t); pv++)
+        {
+            int p = ctrls[c].pvs[pv].p;
 
-			switch (pv)
-			{
-			case RED:
-			case GREEN:
-			case BLUE:
-			case RELAY:
-				pinMode(p, OUTPUT);
-				digitalWrite(p, LOW);
-				break;
-			case SWITCH:
-				pinMode(p, INPUT_PULLUP);
-				ctrls[c].pvs[pv].v = -1;
-				break;
-			default:
-				break;
-			}
-		}
-	}
+            switch (pv)
+            {
+            case RED:
+            case GREEN:
+            case BLUE:
+            case RELAY:
+                pinMode(p, OUTPUT);
+                digitalWrite(p, LOW);
+                break;
+            case SWITCH:
+                pinMode(p, INPUT_PULLUP);
+                ctrls[c].pvs[pv].v = -1;
+                break;
+            default:
+                break;
+            }
+        }
+    }
 }
 
 void buttonDown(int c)
 {
-	ctrls[c].pvs[RELAY].v = !ctrls[c].pvs[RELAY].v;
-	digitalWrite(ctrls[c].pvs[RELAY].p, ctrls[c].pvs[RELAY].v);
+    ctrls[c].pvs[RELAY].v = !ctrls[c].pvs[RELAY].v;
+    digitalWrite(ctrls[c].pvs[RELAY].p, ctrls[c].pvs[RELAY].v);
 
-	analogWrite(ctrls[c].pvs[BLUE].p, 0);
-	analogWrite(ctrls[c].pvs[ctrls[c].pvs[RELAY].v].p, ctrls[c].pvs[ctrls[c].pvs[RELAY].v].v);
+    analogWrite(ctrls[c].pvs[BLUE].p, 0);
+    analogWrite(ctrls[c].pvs[ctrls[c].pvs[RELAY].v].p, ctrls[c].pvs[ctrls[c].pvs[RELAY].v].v);
 }
 
 void buttonUp(int c)
 {
-	analogWrite(ctrls[c].pvs[ctrls[c].pvs[RELAY].v].p, 0);
-	analogWrite(ctrls[c].pvs[BLUE].p, ctrls[c].pvs[BLUE].v);
+    analogWrite(ctrls[c].pvs[ctrls[c].pvs[RELAY].v].p, 0);
+    analogWrite(ctrls[c].pvs[BLUE].p, ctrls[c].pvs[BLUE].v);
 }
 
 void setup()
 {
-	reset();
+    reset();
 }
 
 void loop()
 {
-	for (int c = 0; c < sizeof(ctrls) / sizeof(ctrl_t); c++)
-	{
-		int v = digitalRead(ctrls[c].pvs[SWITCH].p);
-		if (v != ctrls[c].v)
-		{
-			v == LOW ? buttonDown(c) : buttonUp(c);
-			ctrls[c].v = v;
-		}
-	}
+    for (int c = 0; c < sizeof(ctrls) / sizeof(ctrl_t); c++)
+    {
+        int v = digitalRead(ctrls[c].pvs[SWITCH].p);
+        if (v != ctrls[c].v)
+        {
+            v == LOW ? buttonDown(c) : buttonUp(c);
+            ctrls[c].v = v;
+        }
+    }
 }
