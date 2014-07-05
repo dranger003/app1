e37b50ccad1e9cdb8e6a39f5807b62931b744f9a
Daniel Ranger <daniel@formatique.com>
2014-07-05 14:02:07 -0400

    Add idle breathing.

--- app1.cpp
+++ app1.cpp
@@ -8,6 +8,7 @@ typedef struct
 {
     int p;
     int v;
+	int s;
 } pv_t;
 
 typedef struct
@@ -20,25 +21,28 @@ ctrl_t ctrls[] =
 {
     {
         {
-            { A0, 155 }, // R
-            { A1, 247 }, // G
-            { A4, 120 }, // B
-            { D0, LOW }, // S
-            { D1, LOW }, // R
+            { A0, 155, 0 }, // R
+			{ A1, 247, 0 }, // G
+			{ A4, 240, 0 }, // B
+			{ D0, LOW, 0 }, // S
+			{ D1, LOW, 0 }, // R
         },
         LOW,
     },
     {
         {
-            { A5, 155 }, // R
-            { A6, 247 }, // G
-            { A7, 120 }, // B
-            { D2, LOW }, // S
-            { D3, LOW }, // R
+			{ A5, 155, 0 }, // R
+			{ A6, 247, 0 }, // G
+			{ A7, 240, 0 }, // B
+			{ D2, LOW, 0 }, // S
+			{ D3, LOW, 0 }, // R
         },
         LOW,
     },
 };
+
+int v = 0, d = 1;
+unsigned long t = millis();
 
 void reset()
 {
@@ -75,14 +79,43 @@ void buttonDown(int c)
     ctrls[c].pvs[RELAY].v = !ctrls[c].pvs[RELAY].v;
     digitalWrite(ctrls[c].pvs[RELAY].p, ctrls[c].pvs[RELAY].v);
 
+	ctrls[c].pvs[BLUE].s = 1;
     analogWrite(ctrls[c].pvs[BLUE].p, 0);
-    analogWrite(ctrls[c].pvs[ctrls[c].pvs[RELAY].v].p, ctrls[c].pvs[ctrls[c].pvs[RELAY].v].v);
+
+	analogWrite(ctrls[c].pvs[ctrls[c].pvs[RELAY].v].p, ctrls[c].pvs[ctrls[c].pvs[RELAY].v].v);
 }
 
 void buttonUp(int c)
 {
     analogWrite(ctrls[c].pvs[ctrls[c].pvs[RELAY].v].p, 0);
-    analogWrite(ctrls[c].pvs[BLUE].p, ctrls[c].pvs[BLUE].v);
+
+	analogWrite(ctrls[c].pvs[BLUE].p, v);
+	ctrls[c].pvs[BLUE].s = 0;
+}
+
+void breathe(int c)
+{
+	if (ctrls[c].pvs[BLUE].s)
+		return;
+
+	int nt = millis();
+	if (t != nt)
+	{
+		t = nt;
+
+		!d ? --v : ++v;
+		if (v < 0 || v > ctrls[c].pvs[BLUE].v)
+		{
+			if (v < 0)
+				v = 0;
+			else if (v > ctrls[c].pvs[BLUE].v)
+				v = ctrls[c].pvs[BLUE].v;
+
+			d = !d;
+		}
+
+		analogWrite(ctrls[c].pvs[BLUE].p, v);
+	}
 }
 
 void setup()
@@ -94,7 +127,9 @@ void loop()
 {
     for (int c = 0; c < sizeof(ctrls) / sizeof(ctrl_t); c++)
     {
-        int v = digitalRead(ctrls[c].pvs[SWITCH].p);
+		breathe(c);
+
+		int v = digitalRead(ctrls[c].pvs[SWITCH].p);
         if (v != ctrls[c].v)
         {
             v == LOW ? buttonDown(c) : buttonUp(c);
